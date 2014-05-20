4fd3f8540f4653419ec7207d1bbd7b6dca41ea0a
Nick Meinhold <nick.meinhold@gmail.com>
2014-05-21 08:07:19 +1000

    Added images and user DTO.

---
+++ creativesuburbs/src/com/creativesuburbs/prototype/shared/UserDTO.java
@@ -0,0 +1,15 @@
+package com.creativesuburbs.prototype.shared;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String userId; 
+	
+	public UserDTO() 
+	{
+		
+	}
+
+}
